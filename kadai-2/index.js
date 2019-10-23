b7594c6a358ff0f61306bab068b09764c5cf95ce
Minori-Inamura <kougetu8@gmail.com>
2019-10-23 05:20:13 +0000

    課題2をいろいろ編集

--- kadai-2/index.js
+++ kadai-2/index.js
@@ -1,5 +1,5 @@
-const firstName = 'Kaho'
-const familyName = 'Sugiyama'
+const firstName = 'Minori'
+const familyName = 'Inamura'
 console.log(firstName + familyName)
 
 const sum = 1+2+3+4+5+6+7+8+9+10
@@ -12,6 +12,11 @@ console.log(hasCat || hasDog)
 
 console.log(hasDog && hasCat)
 //犬は飼っていないが猫は飼っている
-console.log(!hasDog && hasCat)
+console.log(hasDog && hasCat)
 //犬も猫も飼っていない
-console.log(!hasDog && !hasCat)
+console.log(hasDog && !hasCat)
+
+const cat = 1
+const cats = 2
+//ﾈｺﾁｬﾝ......ﾈｺﾁｬﾝﾀｸｻﾝ......！
+console.log(cat + cats)
