dbe1bd1b0d7675812e1030dc52efef0954552a50
Minori-Inamura <kougetu8@gmail.com>
2019-10-23 05:02:09 +0000

    ’課題1と課題2フォルダの内容を編集’

--- kadai-1/index.js
+++ kadai-1/index.js
@@ -1,2 +1,12 @@
 console.log('hello!')
 
+//実習
+const name = 'Nekochan'
+console.log(name)
+//実習2
+let name1 = 'SHJ'
+console.log(name1)
+name1 = 'BFJZ'
+console.log(name1)
+const name2 = 'LSY'
+console.log(name2)
--- kadai-2/index.js
+++ kadai-2/index.js
@@ -1,10 +1,17 @@
-//実習
-const name = 'Nekochan'
-console.log(name)
-//実習2
-let name1 = 'SHJ'
-console.log(name1)
-name1 = 'BFJZ'
-console.log(name1)
-const name2 = 'LSY'
-console.log(name2)
+const firstName = 'Kaho'
+const familyName = 'Sugiyama'
+console.log(firstName + familyName)
+
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasCat = true
+const hasDog = false
+
+console.log(hasCat || hasDog)
+
+console.log(hasDog && hasCat)
+//犬は飼っていないが猫は飼っている
+console.log(!hasDog && hasCat)
+//犬も猫も飼っていない
+console.log(!hasDog && !hasCat)
