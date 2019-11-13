2720af9329136af2f7dbc097f1c953385c608930
Minori-Inamura <kougetu8@gmail.com>
2019-11-13 05:33:00 +0000

    ’課題５を編集’

--- kadai-5/index.js
+++ kadai-5/index.js
@@ -1,8 +1,8 @@
-const person = {
+const profile = {
     name:'MinoriInada',
     age: 24,
     hobby:'watch Budaixi,Taiwanese puppet show',
-    major:'Chinese literature'
+    major:'Chinese Literature'
 }
 
-console.log(person)
+
