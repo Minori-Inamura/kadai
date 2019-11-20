0fe95c2ca0834e7abcc814fd43a82982a992c107
Minori-Inamura <kougetu8@gmail.com>
2019-11-20 05:06:39 +0000

    課題5を編集

--- kadai-5/index.js
+++ kadai-5/index.js
@@ -3,6 +3,13 @@ const profile = {
     age: 24,
     hobby:'watch Budaixi,Taiwanese puppet show',
     major:'Chinese Literature'
+};
+
+const keys = Object.keys(profile);
+
+for(let i = 0; i < keys.length; i = i + 1){
+    const key = keys[i];
+    const value = profile[key];
+    const text = "私の" + key + "は" + value + "です。";
+    console.log(profile[text])
 }
-
-
