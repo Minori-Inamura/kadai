5084d4c447412032e3fb50f31e220bd9b75b628d
Minori-Inamura <kougetu8@gmail.com>
2019-12-08 09:49:08 +0000

    課題7を追加

--- kadai-6/index-1.js
+++ kadai-6/index-1.js
@@ -1,1 +1,8 @@
-const obl1 = { func: () => console.log('Hi!')}
+const obl1 = "abc"
+const obj2 = 123
+const obj3 = false
+const obj4 = { a: 123}
+const obj5 = { func: () => console.log('Hi!')}
+const obj6 = { property: undefined }
+
+console.log
---
+++ kadai-6/kadai-2.js
@@ -0,0 +1,12 @@
+const str1 = "abc"
+const str2 = "'abc'"
+const str3 = '"abc"'
+const str4 = '{"a": undefined}'
+const str5 = '{"a": 123}'
+const str6 = '{a: 123}'
+
+try{
+    console.log(JSON.parse(str1))
+} catch (error) {
+    console.log(error)
+}
