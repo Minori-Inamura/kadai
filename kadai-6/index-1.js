9c54659822fd3e8e76e8c1af317062bb45a50024
Minori-Inamura <kougetu8@gmail.com>
2019-12-18 05:30:28 +0000

    課題６編集

--- kadai-6/index-1.js
+++ kadai-6/index-1.js
@@ -4,5 +4,9 @@ const obj3 = false
 const obj4 = { a: 123}
 const obj5 = { func: () => console.log('Hi!')}
 const obj6 = { property: undefined }
+const obj7 = {a: "aaa", b: { c: "ccc", d: undefined} }
 
-console.log
+console.log(JSON.stringify(obj1))
+console.log(JSON.stringify(obj2))
+console.log(JSON.stringify(obj5))
+console.log(JSON.stringify(obj7))
