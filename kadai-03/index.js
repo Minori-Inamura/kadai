5ea2e8820df6861a6947f975d68c70b981abb92a
Minori-Inamura <kougetu8@gmail.com>
2020-01-15 05:30:03 +0000

    課題３編集

--- kadai-03/index.js
+++ kadai-03/index.js
@@ -8,3 +8,17 @@ if (day % 3 === 0){
 } else if (day % 3 === 2){
     console.log('あなたは奔放な性格です')
 }
+//Example
+
+// 生まれた日を入力
+//const day = parseInt(process.argv[2])
+
+//if (day % 4 === 0) {
+ //   console.log('あなたはaaaaな性格です')
+//} else if(day % 4 === 1) {
+   // console.log('あなたはbbbな性格です')
+//} else if(day % 4 === 2) {
+  //  console.log('dddd')
+//} else {
+//    console.log('あなたはcccな性格です')
+//}
