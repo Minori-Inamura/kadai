74d3bda9d4038cf1fbd467e4172162ecc5af711f
Minori-Inamura <kougetu8@gmail.com>
2020-01-15 05:17:53 +0000

    課題３を編集

---
+++ kadai-11/index.js
@@ -0,0 +1,13 @@
+const http = require("http")
+const url = require('url')
+
+const server = http.createServer((request, response) => {
+
+
+    const 
+   // response.setHeader('Access-Control-Allow-Origin',)
+    res.write('hello')
+	res.end()
+})
+server.listen(4000)
+console.log('Listening port 4000…')
