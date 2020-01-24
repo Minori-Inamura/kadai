20e46993a43b61de36d69f78b175c4b440b74ed5
Minori-Inamura <kougetu8@gmail.com>
2020-01-24 18:00:16 +0000

    some fix

--- kadai-06/index-1.js
+++ kadai-06/index-1.js
@@ -1,3 +1,7 @@
+//課題6-1
+//次のオブジェクトがJSON.stringifyできるか
+// =JavaScript オブジェクトを JSON 文字列に変換できるか確認する
+
 const obl1 = "abc"
 const obj2 = 123
 const obj3 = false
@@ -5,6 +9,24 @@ const obj4 = { a: 123}
 const obj5 = { func: () => console.log('Hi!')}
 const obj6 = { property: undefined }
 const obj7 = {a: "aaa", b: { c: "ccc", d: undefined} }
+
+
+//const cat = {
+  // "species": "cat",
+   //"name": "Tama",
+   //"age": 2,
+   //"sex": "male"
+ //}JSON.stringify(cat)
+
+//解答例
+
+const obj1 = "abc"
+const obj2 = 123
+const obj3 = false
+const obj4 = { a: 123 }
+const obj5 = { func: () => console.log('Hi!') }
+const obj6 = { property: undefined }
+const obj7 = {a: "aaa", b: { c: "ccc", d: undefined } }
 
 console.log(JSON.stringify(obj1))
 console.log(JSON.stringify(obj2))
