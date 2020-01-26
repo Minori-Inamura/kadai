5d2c348e69fd341b0237ffc8d94e17a5e850e925
Minori-Inamura <kougetu8@gmail.com>
2020-01-26 14:30:08 +0000

    fix kadai-11

--- kadai-11/index.js
+++ kadai-11/index.js
@@ -2,11 +2,21 @@ const http = require("http")
 const url = require('url')
 
 const server = http.createServer((request, response) => {
-
     const res = response;
-    //const 
-   // response.setHeader('Access-Control-Allow-Origin',)
-    res.write('hello')
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    const num = Math.random()
+    if(num < 0.5){
+        res.write('吉です')
+    } else if (num < 0.8){
+        res.write('中吉です')
+    } else if (num < 0.95){
+        res.write('大吉です')
+    } else {
+        res.write('凶')
+    }
+
 	res.end()
 })
 server.listen(4000)
