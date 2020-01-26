0be0750ef91998a38aa9b14001706d7d3f7e7938
Minori-Inamura <kougetu8@gmail.com>
2020-01-26 06:36:10 +0000

    some fix

--- kadai-06/index-1.js
+++ kadai-06/index-1.js
@@ -2,13 +2,13 @@
 //次のオブジェクトがJSON.stringifyできるか
 // =JavaScript オブジェクトを JSON 文字列に変換できるか確認する
 
-const obl1 = "abc"
-const obj2 = 123
-const obj3 = false
-const obj4 = { a: 123}
-const obj5 = { func: () => console.log('Hi!')}
-const obj6 = { property: undefined }
-const obj7 = {a: "aaa", b: { c: "ccc", d: undefined} }
+//const obl1 = "abc"
+//const obj2 = 123
+//const obj3 = false
+//const obj4 = { a: 123}
+//const obj5 = { func: () => console.log('Hi!')}
+//const obj6 = { property: undefined }
+//const obj7 = {a: "aaa", b: { c: "ccc", d: undefined} }
 
 
 //const cat = {
@@ -21,14 +21,17 @@ const obj7 = {a: "aaa", b: { c: "ccc", d: undefined} }
 //解答例
 
 const obj1 = "abc"
-const obj2 = 123
+const obj2 = 123 + ' is string'
 const obj3 = false
 const obj4 = { a: 123 }
 const obj5 = { func: () => console.log('Hi!') }
 const obj6 = { property: undefined }
 const obj7 = {a: "aaa", b: { c: "ccc", d: undefined } }
 
-console.log(JSON.stringify(obj1))
-console.log(JSON.stringify(obj2))
-console.log(JSON.stringify(obj5))
-console.log(JSON.stringify(obj7))
+console.log('obj1: ' + JSON.stringify(obj1))
+console.log('obj2: ' + JSON.stringify(obj2))
+console.log('obj3: ' + JSON.stringify(obj3))
+console.log('obj4: ' + JSON.stringify(obj4))
+console.log('obj5: ' + JSON.stringify(obj5))
+console.log('obj6: ' + JSON.stringify(obj6))
+console.log('obj7: ' + JSON.stringify(obj7))
