6d7cde5e27619d407dc1fe3be95b4c2ed05f5243
Minori-Inamura <kougetu8@gmail.com>
2020-01-26 06:47:48 +0000

    rename

--- kadai-06/index-2.js
+++ kadai-06/index-2.js
@@ -1,12 +1,13 @@
 //次のオブジェクトが JSON.stringify できるかどうか
 // === JSON 文字列を JavaScript オブジェクトに変換できるかを確認する
-const str1 = "abc"
-const str2 = "'abc'"
-const str3 = '"abc"'
+
+const str1 = 'abc'
+
+const str2 = 1//"'abc'"
+const str3 = 2//'"abc"'
 const str4 = '{"a": undefined}'
 const str5 = '{"a": 123}'
 const str6 = '{a: 123}'
-
 
 //解答例
 // str1 のパース結果
@@ -17,6 +18,8 @@ try {
     // abc という文字列は正しい JSON ではないから。
     // 正しくは "abc"
 }
+
+return;
 
 // str2 のパース結果
 try {
